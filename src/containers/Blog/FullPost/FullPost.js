6f3cc28261d9d80270799567eb28464a23b65912
nixdie <nixdie@yandex.ru>
2019-02-20 15:27:46 +0700

    nested routes

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -10,9 +10,16 @@ class FullPost extends Component {
   };
 
   componentDidMount() {
+    this.loadData();
+  }
 
+  componentDidUpdate () {
+    this.loadData();
+  }
+
+  loadData () {
     if (this.props.match.params.id) {
-      if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+      if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id != this.props.match.params.id)) {
         axios
           .get('/posts/' + this.props.match.params.id)
           .then(response => {
@@ -20,17 +27,18 @@ class FullPost extends Component {
           });
       }
     }
+
   }
 
   postDeleteHandler = () => {
-    axios.delete('/posts/' + this.props.id)
+    axios.delete('/posts/' + this.props.match.params.id)
       .then(response => console.log(response))
 
   }
 
   render() {
     let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
-    if (this.props.id) {
+    if (this.props.match.params.id) {
       post = <p style={{ textAlign: 'center' }}>Loading... ¯\_(ツ)_/¯</p>;
     }
     if (this.state.loadedPost) {
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
 import Post from '../../../components/Post/Post';
-import { Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import FullPost from '../FullPost/FullPost';
+// import { Link } from 'react-router-dom';
 import './Posts.css';
 
 class Posts extends Component {
@@ -10,7 +12,6 @@ class Posts extends Component {
   };
 
   componentDidMount = () => {
-
     axios
       .get('/posts')
       .then((response) => {
@@ -24,13 +25,14 @@ class Posts extends Component {
         this.setState({ posts: updatedPosts });
       })
       .catch((error) => {
-        console.log(error)
+        console.log(error);
         // this.setState({ error: true });
       });
   };
 
   postSelectedHandler = (id) => {
-    this.setState({ selectedPostId: id });
+    this.props.history.push({ pathname: '/posts/' + id });
+    // this.props.history.push('/' + id)
   };
 
   render() {
@@ -40,18 +42,28 @@ class Posts extends Component {
     if (!this.state.error) {
       posts = this.state.posts.map((post) => {
         return (
-          <Link to={'/' + post.id} key={post.id}>
-            <Post
-              title={post.title}
-              author={post.author}
-              clicked={() => this.postSelectedHandler(post.id)}
-            />
-          </Link>
+          // <Link to={'/' + post.id} key={post.id}>
+          <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />
+          // </Link>
         );
       });
     }
 
-    return <section className="Posts">{posts}</section>;
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <Route
+          path={this.props.match.url + '/:id'}
+          exact
+          component={FullPost}
+        />
+      </div>
+    );
   }
 }
 
