70febf0b27e8744018b5b3ac84bc95820675c1ab
nixdie <nixdie@yandex.ru>
2019-02-20 15:55:03 +0700

    handling 404 case

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -46,7 +46,8 @@ class Blog extends Component {
         <Switch>
           { this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
           <Route path="/posts" component={Posts} />
-          <Redirect from="/" to="/posts" />
+          <Route render={() => <h1 style={{textAlign: 'center'}}>Not found ಠ_ಠ</h1>}/>
+          {/* <Redirect from="/" to="/posts" /> */}
         </Switch>
       </div>
     );
