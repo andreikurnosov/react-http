4354813540e07b37156e2a0adb3f31af21ba7e22
nixdie <nixdie@yandex.ru>
2019-02-20 13:32:52 +0700

    preparing for routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,67 +1,29 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import axios from '../../axios';
-
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+// import axios from '../../axios';
 import './Blog.css';
+import Posts from './Posts/Posts';
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: null,
-    error: false
-  };
 
-  componentDidMount = () => {
-    axios
-      .get('/posts')
-      .then((response) => {
-        const posts = response.data.slice(0, 4);
-        const updatedPosts = posts.map((post) => {
-          return {
-            ...post,
-            author: 'nixdie'
-          };
-        });
-        this.setState({ posts: updatedPosts });
-      })
-      .catch((error) => {
-        // console.log(error)
-        this.setState({ error: true });
-      });
-  };
 
-  postSelectedHandler = (id) => {
-    this.setState({ selectedPostId: id });
-  };
   render() {
-    let posts = (
-      <p style={{ textAlign: 'center' }}>Something went wrong ¯\_(ツ)_/¯</p>
-    );
-    if (!this.state.error) {
-      posts = this.state.posts.map((post) => {
-        return (
-          <Post
-            title={post.title}
-            key={post.id}
-            author={post.author}
-            clicked={() => this.postSelectedHandler(post.id)}
-          />
-        );
-      });
-    }
 
     return (
       <div>
-        <section className="Posts">{posts}</section>
-        <section>
-          <FullPost id={this.state.selectedPostId} />
-        </section>
-        <section>
-          <NewPost />
-        </section>
+        <header className="Blog">
+          <nav>
+            <ul>
+              <li>
+                <a href="/">Home</a>
+                <a href="/new-post">New Post</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+
+        <Posts />
+
       </div>
     );
   }
