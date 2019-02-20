834f2c18bffa22c13547f2c8aee5cc11f07a54ec
nixdie <nixdie@yandex.ru>
2019-02-20 13:59:37 +0700

    Links to switch pages

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 // import axios from 'axios';
 // import axios from '../../axios';
 import './Blog.css';
 import Posts from './Posts/Posts';
-
+import NewPost from './NewPost/NewPost';
 class Blog extends Component {
   render() {
     return (
@@ -13,14 +13,19 @@ class Blog extends Component {
           <nav>
             <ul>
               <li>
-                <a href="/">Home</a>
-                <a href="/new-post">New Post</a>
+                <Link to="/" >Home</Link>
+                <Link to={{
+                  pathname: '/new-post',
+                  hash: '#submit',
+                  search: '?quick-submit=true'
+                }}>New Post</Link>
               </li>
             </ul>
           </nav>
         </header>
         {/* <Route path="/" exact render={() => <h1>Home</h1>}/> */}
         <Route path="/" exact component={Posts} />
+        <Route path="/new-post" component={NewPost} />
       </div>
     );
   }
